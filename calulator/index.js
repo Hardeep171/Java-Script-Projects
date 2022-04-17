7d276b2e3acc2694914e66f2acd57b4093bf12a8
hardeep <hardeepchoudhary9654@gmail.com>
2022-04-17 05:30:05 +0530

    calculator project

--- calulator/index.js
+++ calulator/index.js
@@ -1,5 +1,54 @@
 var displayArea = document.getElementById("display")
 
-function buttonClicked(digitOrOperator){
-    displayArea.innerText = displayArea.innerText + digitOrOperator
+function isOperator(btnText) {
+    if (btnText == '+' || btnText == '-' || btnText == '/' || btnText == '*' || btnText == '%')
+        return true
+    else
+        return false
+}
+function buttonClicked(btnText) {
+    if(displayArea.innerText.length >=24){
+        displayArea.innerText = "0"
+    }
+    if (btnText != 'clear' && btnText != 'del' && btnText != '=') {
+        if (displayArea.innerText === '0') {
+            // dont do any thing if btn Text is 0 and 00
+            if (btnText !== '00' && btnText !== '0') {
+                if (btnText == '.' || isOperator(btnText)) {
+                    displayArea.innerText = displayArea.innerText + btnText
+                }
+                else {
+                    displayArea.innerText = btnText
+                }
+            }
+        }
+        else {
+            if (isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(btnText)) {
+                displayArea.innerText = displayArea.innerText.slice(0, -1) + btnText
+            }
+            else {
+                displayArea.innerText = displayArea.innerText + btnText
+            }
+        }
+    }
+    else {
+        if (btnText === 'clear') {
+            displayArea.innerText = '0'
+        }
+        else if (btnText === 'del') {
+            if (displayArea.innerText.length == '1') {
+                displayArea.innerText = '0'
+            }
+            else if (displayArea.innerText.length > '0')
+                displayArea.innerText = displayArea.innerText.slice(0, -1)
+        }
+        else if(btnText === '='){
+            try{
+               displayArea.innerText = eval(displayArea.innerText)
+            }
+            catch(error){
+               displayArea.innerText = '0'
+            }
+        }
+    }
 }
